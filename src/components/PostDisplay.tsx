b3fae26d5f2521b8588371b2f69a08755789e709
Akshat <masterakshata@gmail.com>
2020-09-25 20:28:47 +0530

    added author name in post

--- src/components/PostDisplay.tsx
+++ src/components/PostDisplay.tsx
@@ -1,10 +1,14 @@
-import { Stack, Box, Heading, Text } from '@chakra-ui/core';
+import { Stack, Box, Heading, Text, Flex } from '@chakra-ui/core';
 import React from 'react'
 import { Post } from '../generated/graphql';
-function Feature({ title, desc, ...rest }) {
+function Feature({ title, desc, author, ...rest }) {
     return (
         <Box p={5} shadow="md" borderWidth="1px" {...rest}>
-            <Heading fontSize="xl">{title}</Heading>
+            <Flex justifyContent="space-between">
+                <Heading fontSize="xl">{title}</Heading>
+                <Heading size="xs">-{author}</Heading>
+            </Flex>
+            
             <Text mt={4}>{desc}</Text>
         </Box>
     );
@@ -19,6 +23,7 @@ export const PostDisplay: React.FC<PostDisplayProps> = ({ post }) => {
         <Stack mb={2} spacing={8} key={post.id}>
             <Feature
                 title={post.title}
+                author={post.creator!.username}
                 desc={post.text.slice(0, 60)}
             />
         </Stack>
